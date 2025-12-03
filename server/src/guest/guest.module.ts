8b53a398026b27c48e6e8713e8c427e6018a809f
Jiya-Singh-sudo <ayij.3241@gmail.com>
2025-12-03 11:27:04 +0530

    remove guest

--- server/src/guest/guest.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GuestService } from './guest.service';
-import { GuestController } from './guest.controller';
-
-@Module({
-  providers: [GuestService],
-  controllers: [GuestController]
-})
-export class GuestModule {}
