71af54cb7673c5f1835054ddd2ef13de35922cdc
Jiya-Singh-sudo <ayij.3241@gmail.com>
2026-01-14 16:24:16 +0530

    transport

--- server/src/guest-transport/guest-transport.module.ts
+++ server/src/guest-transport/guest-transport.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GuestTransportController } from './guest-transport.controller';
 import { GuestTransportService } from './guest-transport.service';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [GuestTransportController],
   providers: [GuestTransportService]
 })
