b9269c0c821147d795bd551f5e2e565f3cfb82bb
Jiya-Singh-sudo <ayij.3241@gmail.com>
2026-01-14 10:48:11 +0530

    guest edit

--- server/src/guests/dto/update-guests.dto.ts
+++ server/src/guests/dto/update-guests.dto.ts
@@ -1,8 +1,8 @@
 export class UpdateGuestDto {
   guest_name?: string;
   guest_name_local_language?: string;
-  mobile?: string;
-  alternate_mobile?: string;
+  guest_mobile?: string;
+  guest_alternate_mobile?: string;
   guest_address?: string;
   email?: string;
 }
